9b963ff00b282a9fa130b65c511633a45bb905d3
nol0n <sharapovegor2003@gmail.com>
2023-10-27 15:11:18 +0300

    change M_PI to my PI values

--- tasks/task_1/sharapov_g_trapezoidal_rule/main.cpp
+++ tasks/task_1/sharapov_g_trapezoidal_rule/main.cpp
@@ -48,6 +48,7 @@ TEST(Integral_Trapezoidal_Rule_MPI, Test_PI) {
     int ProcRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
+    double PI = 3.14159265358979323846;
     double err = 0.00001;
     double a = 0.0;
     double b = 1.0;
@@ -60,7 +61,7 @@ TEST(Integral_Trapezoidal_Rule_MPI, Test_PI) {
     double sum = integralTrapezoidalParallel(a, b, n, function);
 
     if (ProcRank == 0) {
-        ASSERT_LT(abs(M_PI - sum), err);
+        ASSERT_LT(abs(PI - sum), err);
     }
 }
 
@@ -68,9 +69,10 @@ TEST(Integral_Trapezoidal_Rule_MPI, Test_PI_2) {
     int ProcRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
+    double PI_2 = 1.57079632679489661923;
     double err = 0.00001;
     double a = 0.0;
-    double b = M_PI_2;
+    double b = PI_2;
     int64_t n = 1000000;
 
     auto function = [](double x) {
@@ -80,7 +82,7 @@ TEST(Integral_Trapezoidal_Rule_MPI, Test_PI_2) {
     double sum = integralTrapezoidalParallel(a, b, n, function);
 
     if (ProcRank == 0) {
-        ASSERT_LT(abs(M_PI_2 - sum), err);
+        ASSERT_LT(abs(PI_2 - sum), err);
     }
 }
 
@@ -88,6 +90,7 @@ TEST(Integral_Trapezoidal_Rule_MPI, Test_PI_4) {
     int ProcRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
+    double PI_4 = 0.78539816339744830962;
     double err = 0.00001;
     double a = 0.0;
     double b = 1.0;
@@ -100,7 +103,7 @@ TEST(Integral_Trapezoidal_Rule_MPI, Test_PI_4) {
     double sum = integralTrapezoidalParallel(a, b, n, function);
 
     if (ProcRank == 0) {
-        ASSERT_LT(abs(M_PI_4 - sum), err);
+        ASSERT_LT(abs(PI_4 - sum), err);
     }
 }
 
