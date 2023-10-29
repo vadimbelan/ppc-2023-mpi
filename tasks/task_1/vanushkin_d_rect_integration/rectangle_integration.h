2dc2c67ada462ef855512ab62598351beef232ee
Dmitry <xtopz1337@gmail.com>
2023-10-29 19:51:33 +0300

    change: add functions prototypes

--- tasks/task_1/vanushkin_d_rect_integration/rectangle_integration.h
+++ tasks/task_1/vanushkin_d_rect_integration/rectangle_integration.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <iostream>
+#include <functional>
+
+using IntegrateFunction = const std::function<double(double)>&;
+
+double RectangleIntegrateSequential(IntegrateFunction f, double lowerBound, double upperBound, double step);
+
+double RectangleIntegrateParallel(IntegrateFunction f, double lowerBound, double upperBound, double step);
