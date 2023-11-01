110deef59d951a17bcdb87fb6fc3a5f8a3beece1
nol0n <sharapovegor2003@gmail.com>
2023-11-01 23:44:57 +0300

    done

--- tasks/task_2/sharapov_g_seidel_method/main.cpp
+++ tasks/task_2/sharapov_g_seidel_method/main.cpp
@@ -3,8 +3,44 @@
 #include <gtest/gtest.h>
 #include "task_2/sharapov_g_seidel_method/seidel_method.h"
 
-TEST(Seidel_Method_MPI, Test_1) {
-    ASSERT_EQ(1, 1);
+TEST(Seidel_Method_MPI, Test_5) {
+    int size = 5;
+
+    bool test = seidelMethod(size, 0.000001);
+
+    ASSERT_TRUE(test);
+}
+
+TEST(Seidel_Method_MPI, Test_25) {
+    int size = 25;
+
+    bool test = seidelMethod(size, 0.000001);
+
+    ASSERT_TRUE(test);
+}
+
+TEST(Seidel_Method_MPI, Test_50) {
+    int size = 50;
+
+    bool test = seidelMethod(size, 0.000001);
+
+    ASSERT_TRUE(test);
+}
+
+TEST(Seidel_Method_MPI, Test_75) {
+    int size = 75;
+
+    bool test = seidelMethod(size, 0.000001);
+
+    ASSERT_TRUE(test);
+}
+
+TEST(Seidel_Method_MPI, Test_100) {
+    int size = 100;
+
+    bool test = seidelMethod(size, 0.000001);
+
+    ASSERT_TRUE(test);
 }
 
 int main(int argc, char** argv) {
--- tasks/task_2/sharapov_g_seidel_method/seidel_method.h
+++ tasks/task_2/sharapov_g_seidel_method/seidel_method.h
@@ -2,4 +2,8 @@
 #ifndef TASKS_TASK_2_SHARAPOV_G_SEIDEL_METHOD_SEIDEL_METHOD_H_
 #define TASKS_TASK_2_SHARAPOV_G_SEIDEL_METHOD_SEIDEL_METHOD_H_
 
+double* generateMatrix(size_t size);
+void printMatrix(double* matrix, size_t size);
+bool seidelMethod(size_t n, double eps);
+
 #endif  // TASKS_TASK_2_SHARAPOV_G_SEIDEL_METHOD_SEIDEL_METHOD_H_
