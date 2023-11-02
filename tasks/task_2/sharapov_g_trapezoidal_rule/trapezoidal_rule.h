3ffa2d7e8b4f4172ccc022dade8ed2dad7b6209e
nol0n <sharapovegor2003@gmail.com>
2023-11-02 16:54:13 +0300

    Revert "changed directory now for sure"
    This reverts commit 32ba1b2eb08324eece9adf2a051f16121fd988df.

--- tasks/task_2/sharapov_g_trapezoidal_rule/trapezoidal_rule.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Sharapov Georgiy
-#ifndef TASKS_TASK_1_SHARAPOV_G_TRAPEZOIDAL_RULE_TRAPEZOIDAL_RULE_H_
-#define TASKS_TASK_1_SHARAPOV_G_TRAPEZOIDAL_RULE_TRAPEZOIDAL_RULE_H_
-
-#include <functional>
-
-double integralTrapezoidalParallel(double a, double b, int64_t l, std::function<double(double)> func);
-
-#endif  // TASKS_TASK_1_SHARAPOV_G_TRAPEZOIDAL_RULE_TRAPEZOIDAL_RULE_H_
