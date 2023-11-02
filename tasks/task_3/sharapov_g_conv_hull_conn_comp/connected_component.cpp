0471686b1c106b698c7882a0b7110a6838d93555
nol0n <sharapovegor2003@gmail.com>
2023-11-02 23:48:29 +0300

    nade mistake

--- tasks/task_3/sharapov_g_conv_hull_conn_comp/connected_component.cpp
+++ tasks/task_3/sharapov_g_conv_hull_conn_comp/connected_component.cpp
@@ -5,3 +5,42 @@
 #include <iostream>
 #include <algorithm>
 #include "task_3/sharapov_g_conv_hull_conn_comp/connected_component.h"
+
+int* marking(bool* picture, size_t n) {
+    int ProcRank, ProcNum;
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+
+    //  declaration of variables
+    int *ProcSizes = new int[ProcNum]();
+    for (int i = 0; i < ProcNum; i++) {
+        ProcSizes[i] = n / ProcNum + (i < n % ProcNum ? 1 : 0);
+    }
+
+    int *rows = new int[ProcSizes[ProcRank] * n]();
+
+    //  sending rows to corresponding processors
+    if (ProcRank == 0) {
+        int *PackedRows = new int[n * n];
+        for (int row = 0; row < n; row++) {
+            for (int elem = 0; elem < n; elem++) {
+                PackedRows[(ProcSizes[row % ProcNum] * n) + (row / ProcNum) + elem] = picture[(row * n) + elem];
+            }
+        }
+
+        //  DEBUG --------------------------------------
+
+        for (int row = 0; row < n; row++) {
+            for (int elem = 0; elem < n; elem++) {
+                std::cout << PackedRows[row * n + elem];
+            }
+            std::cout << std::endl;
+        }
+
+        //  DEBUG --------------------------------------
+    } else {
+        std::cout << "";
+    }
+
+    return nullptr;
+}
--- tasks/task_3/sharapov_g_conv_hull_conn_comp/main.cpp
+++ tasks/task_3/sharapov_g_conv_hull_conn_comp/main.cpp
@@ -5,5 +5,38 @@
 #include "task_3/sharapov_g_conv_hull_conn_comp/convex_hull.h"
 
 int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    int ProcRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    //  DEBUG --------------------------------------
+
+    bool picture[] = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    };
+
+    marking(picture, 8);
+
+    //  DEBUG --------------------------------------
+
+    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+    if (ProcRank != 0) {
+        delete listeners.Release(listeners.default_result_printer());
+    }
+
+    //  int result = RUN_ALL_TESTS();
+    MPI_Finalize();
+
     return 0;
+    //  return result;
 }
+
