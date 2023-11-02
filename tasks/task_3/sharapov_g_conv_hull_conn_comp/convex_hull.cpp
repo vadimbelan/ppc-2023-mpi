50a74da9b955b13c2cbf7e20d08b19fbc085b471
nol0n <sharapovegor2003@gmail.com>
2023-11-02 22:17:10 +0300

    made template

---
+++ tasks/task_3/sharapov_g_conv_hull_conn_comp/convex_hull.cpp
@@ -0,0 +1,7 @@
+// Copyright 2023 Sharapov Georgiy
+#include <mpi.h>
+#include <cmath>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include "task_3/sharapov_g_conv_hull_conn_comp/convex_hull.h"
---
+++ tasks/task_3/sharapov_g_conv_hull_conn_comp/main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2023 Sharapov Georgiy
+#include <mpi.h>
+#include <gtest/gtest.h>
+#include "task_3/sharapov_g_conv_hull_conn_comp/connected_component.h"
+#include "task_3/sharapov_g_conv_hull_conn_comp/convex_hull.h"
+
+int main(int argc, char** argv) {
+    return 0;
+}
