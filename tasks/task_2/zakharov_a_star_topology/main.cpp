93799b1d08050f8fd6bdcdf368ed3399b983a742
Artem <uni744@yandex.ru>
2023-11-07 22:39:15 +0300

    Fix tests

--- tasks/task_2/zakharov_a_star_topology/main.cpp
+++ tasks/task_2/zakharov_a_star_topology/main.cpp
@@ -121,9 +121,6 @@ int main(int argc, char** argv) {
     boost::mpi::environment env(argc, argv);
     boost::mpi::communicator world;
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
-    if (world.rank() != 0) {
-        delete listeners.Release(listeners.default_result_printer());
-    }
+
     return RUN_ALL_TESTS();
 }
