d1c1641381685f79ad7ebf2189df075c53d8dcbd
Ivan <shmelevivan02@yandex.ru>
2023-11-11 00:43:51 +0300

    create tests

--- tasks/task_2/shmelev_i_simple_iter_method/main.cpp
+++ tasks/task_2/shmelev_i_simple_iter_method/main.cpp
@@ -3,7 +3,85 @@
 
 #include "./simple_iter_method.h"
 
-TEST(TESTS, Test_1) { ASSERT_EQ(1, 1); }
+TEST(TESTS, Test_1) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<double> a = {4, -1, -1, 4};
+  std::vector<double> b = {5, 5};
+  double epsilon = 0.001;
+
+  auto par = SimpleIterPar(a, b, epsilon, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    auto seq = SimpleIterSeq(a, b, epsilon);
+    ASSERT_EQ(par, seq);
+  }
+}
+
+TEST(TESTS, Test_2) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<double> a = {4, -1, 0, -1, 4, -1, 0, -1, 4};
+  std::vector<double> b = {5, 5, 10};
+  double epsilon = 0.001;
+
+  auto par = SimpleIterPar(a, b, epsilon, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    auto seq = SimpleIterSeq(a, b, epsilon);
+    ASSERT_EQ(par, seq);
+  }
+}
+
+TEST(TESTS, Test_3) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<double> a = {10, 1, 2, 3, 1, 10, 2, 3, 1, 0, 10, 2, 2, 0, 0, 10};
+  std::vector<double> b = {12, 3, 11, 2};
+  double epsilon = 0.001;
+
+  auto par = SimpleIterPar(a, b, epsilon, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    auto seq = SimpleIterSeq(a, b, epsilon);
+    ASSERT_EQ(par, seq);
+  }
+}
+
+TEST(TESTS, Test_empty) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<double> a{};
+  std::vector<double> b{};
+  double epsilon = 0.001;
+
+  auto par = SimpleIterPar(a, b, epsilon, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    auto seq = SimpleIterSeq(a, b, epsilon);
+    ASSERT_EQ(par, seq);
+  }
+}
+
+TEST(TESTS, Test_5) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<double> a{5, 1, 2, 1, 1, 15, 1, 3, 4, 1, 9, 1, 1, -1, 1, 11};
+  std::vector<double> b{9, 19, 6, 10};
+  double epsilon = 0.001;
+
+  auto par = SimpleIterPar(a, b, epsilon, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    auto seq = SimpleIterSeq(a, b, epsilon);
+    ASSERT_EQ(par, seq);
+  }
+}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- tasks/task_2/shmelev_i_simple_iter_method/simple_iter_method.h
+++ tasks/task_2/shmelev_i_simple_iter_method/simple_iter_method.h
@@ -1,9 +1,15 @@
 // Copyright 2023 Shmelev Ivan
-#ifndef TASKS_TASK_1_SHMELEV_I_MAX_OF_MATRIX_ELEMENTS_MAX_OF_MATRIX_ELEMENTS_H_
-#define TASKS_TASK_1_SHMELEV_I_MAX_OF_MATRIX_ELEMENTS_MAX_OF_MATRIX_ELEMENTS_H_
+#ifndef TASKS_TASK_2_SHMELEV_I_SIMPLE_ITER_METHOD_SIMPLE_ITER_METHOD_H_
+#define TASKS_TASK_2_SHMELEV_I_SIMPLE_ITER_METHOD_SIMPLE_ITER_METHOD_H_
 
 #include <mpi.h>
 
+#include <cmath>
+#include <vector>
 
+std::vector<double> SimpleIterSeq(std::vector<double> a, std::vector<double> b,
+                                  double epsilon);
+std::vector<double> SimpleIterPar(std::vector<double> a, std::vector<double> b,
+                                  double epsilon, MPI_Comm comm);
 
-#endif  // TASKS_TASK_1_SHMELEV_I_MAX_OF_MATRIX_ELEMENTS_MAX_OF_MATRIX_ELEMENTS_H_
+#endif  // TASKS_TASK_2_SHMELEV_I_SIMPLE_ITER_METHOD_SIMPLE_ITER_METHOD_H_
