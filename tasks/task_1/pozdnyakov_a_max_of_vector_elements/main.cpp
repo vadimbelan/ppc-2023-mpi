edcd3db249b3f39ca6d69ee392f6ac9ff726115b
pozvas <pozvas03@mail.ru>
2023-11-10 21:16:28 +0300

    codestyle fixed

--- tasks/task_1/pozdnyakov_a_max_of_vector_elements/main.cpp
+++ tasks/task_1/pozdnyakov_a_max_of_vector_elements/main.cpp
@@ -1,84 +1,85 @@
+// Copyright 2023 Pozdnyakov Vasya
 #include <gtest/gtest.h>
 #include <vector>
 #include "./max_of_vector_elements.h"
 #include <boost/mpi/environment.hpp>
 #include <boost/mpi/communicator.hpp>
 
-TEST(Max_of_vector_elements, Test_1){
+TEST(Max_of_vector_elements, Test_1) {
     boost::mpi::communicator world;
     std::vector<int> global_vec;
     const size_t global_vec_size = 10;
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         global_vec = get_random_vector(global_vec_size);
     }
 
     int global_res = get_max_element(global_vec, global_vec_size);
 
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         int max = *std::max_element(global_vec.begin(), global_vec.end());
         ASSERT_EQ(max, global_res);
     }
 }
 
-TEST(Max_of_vector_elements, Test_2){
+TEST(Max_of_vector_elements, Test_2) {
     boost::mpi::communicator world;
     std::vector<int> global_vec;
     const size_t global_vec_size = 10;
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         global_vec = get_random_vector(global_vec_size, 1);
     }
 
     int global_res = get_max_element(global_vec, global_vec_size);
 
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         int max = *std::max_element(global_vec.begin(), global_vec.end());
         ASSERT_EQ(max, global_res);
     }
 }
 
-TEST(Max_of_vector_elements, Test_3){
+TEST(Max_of_vector_elements, Test_3) {
     boost::mpi::communicator world;
     std::vector<int> global_vec;
     const size_t global_vec_size = 10;
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         global_vec = get_random_vector(global_vec_size, -1000, -1);
     }
 
     int global_res = get_max_element(global_vec, global_vec_size);
 
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         int max = *std::max_element(global_vec.begin(), global_vec.end());
         ASSERT_EQ(max, global_res);
     }
 }
 
-TEST(Max_of_vector_elements, Test_4){
+TEST(Max_of_vector_elements, Test_4) {
     boost::mpi::communicator world;
     std::vector<int> global_vec;
     const size_t global_vec_size = 1;
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         global_vec = {10};
     }
 
     int global_res = get_max_element(global_vec, global_vec_size);
 
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         int max = *std::max_element(global_vec.begin(), global_vec.end());
         ASSERT_EQ(max, global_res);
     }
 }
 
-TEST(Max_of_vector_elements, Test_5){
+TEST(Max_of_vector_elements, Test_5) {
     boost::mpi::communicator world;
     std::vector<int> global_vec;
     const size_t global_vec_size = 5;
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         global_vec = {43, 43, 43, 43, 43};
     }
 
     int global_res = get_max_element(global_vec, global_vec_size);
 
-    if(world.rank() == 0){
+    if(world.rank() == 0) { 
         int max = *std::max_element(global_vec.begin(), global_vec.end());
         ASSERT_EQ(max, global_res);
     }
@@ -95,6 +96,3 @@ int main(int argc, char** argv) {
     }
     return RUN_ALL_TESTS();
 }
-
-
-
--- tasks/task_1/pozdnyakov_a_max_of_vector_elements/max_of_vector_elements.cpp
+++ tasks/task_1/pozdnyakov_a_max_of_vector_elements/max_of_vector_elements.cpp
@@ -1,39 +1,33 @@
+// Copyright 2023 Pozdnyakov Vasya
 #include "./max_of_vector_elements.h"
-#include <boost/mpi/collectives.hpp>
-#include <boost/mpi/communicator.hpp>
-#include <vector>
-#include <random>
 
-std::vector<int> get_random_vector(size_t size, int min_elem, int max_elem){
-    std::random_device dev;
-    std::mt19937 gen(dev());
-    std::uniform_int_distribution<int> distrib(min_elem, max_elem);
+std::vector<int> get_random_vector(size_t size, int min_elem, int max_elem) {
     std::vector<int> vec(size);
-    for (size_t  i = 0; i < size; i++) 
-        vec[i] = distrib(gen); 
+    for (size_t  i = 0; i < size; i++) {
+        vec[i] = min_elem + (std::rand() % (max_elem - min_elem + 1));
+    }
     return vec;
 }
 
-int get_max_element(const std::vector<int>& init_vec, size_t vec_size){
+int get_max_element(const std::vector<int>& init_vec, size_t vec_size) {
     boost::mpi::communicator world;
     const int local_vec_size = vec_size / world.size();
-    if(world.rank() == 0){
-        for(int proc = 1; proc < world.size(); proc++){
+    if(world.rank() == 0) {
+        for(int proc = 1; proc < world.size(); proc++) {
             world.send(proc, 0, init_vec.data() + local_vec_size * proc, local_vec_size);
         }
     }
 
     std::vector<int> local_vec(local_vec_size);
-    if(world.rank() == 0){
+    if(world.rank() == 0) {
         local_vec = std::vector<int>(init_vec.begin(), init_vec.begin() + local_vec_size);
-    }
-    else{
+    } else {
         world.recv(0, 0, local_vec.data(), local_vec_size);
     }
 
     auto local_res = std::max_element(local_vec.begin(), local_vec.end());
     int global_res;
     reduce(world, *local_res, global_res, boost::mpi::maximum<int>(), 0);
-    
+
     return global_res;
 }
