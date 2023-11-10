b7c875ebc1b9860168b0557a8012c1baea476e3b
Danila <alexseev_dnl@mail.ru>
2023-11-11 01:25:19 +0300

    some style change5

--- tasks/task_1/alexseev_d_word_count/count_words.cpp
+++ tasks/task_1/alexseev_d_word_count/count_words.cpp
@@ -1,6 +1,6 @@
 // Copyright 2023 Alexseev Danila
 
-#include "task1/alexseev_d_word_count/count_words.h"
+#include "task/task1/alexseev_d_word_count/count_words.h"
 
 int getRandInt(int leftBorder, int rightBorder) {
     return rand() % (rightBorder - leftBorder + 1) + leftBorder;
