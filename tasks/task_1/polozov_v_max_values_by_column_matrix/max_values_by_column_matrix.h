494ae9ce40d893122a8534a11f007754a95e1941
Mortus19 <polozzzzzov2033@mail.ru>
2023-11-11 19:01:35 +0300

    first task

--- tasks/task_1/polozov_v_max_values_by_column_matrix/max_values_by_column_matrix.h
+++ tasks/task_1/polozov_v_max_values_by_column_matrix/max_values_by_column_matrix.h
@@ -1,10 +1,23 @@
 // Copyright 2023 Polozov Vladislav
 
-#inlude <vector>
+
+
+
+#ifndef TASKS_TASK_1_POLOZOV_V_MAX_VALUES_BY_COLUMN_MATRIX_MAX_VALUES_BY_COLUMN_MATRIX_H_
+#define TASKS_TASK_1_POLOZOV_V_MAX_VALUES_BY_COLUMN_MATRIX_MAX_VALUES_BY_COLUMN_MATRIX_H_
+
+#include <mpi.h>
+#include <climits>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include <random>
 
+std::vector<int> getParallelMax(const std::vector<int> &A, int n, int m);
 
-#ifndef TASKS_TASK_1_POLOZOV_V_MAX_VALUES_BY_COLUM_MATRIX_H
-#define TASKS_TASK_1_POLOZOV_V_MAX_VALUES_BY_COLUM_MATRIX_H
+std::vector<int> getSequentialMax(const std::vector<int> &A, int n, int m);
 
-#endif //TASKS_TASK_1_POLOZOV_V_MAX_VALUES_BY_COLUM_MATRIX_H
+std::vector<int> getRandomMatrix(int n, int m);
+
+
+#endif  // TASKS_TASK_1_POLOZOV_V_MAX_VALUES_BY_COLUMN_MATRIX_MAX_VALUES_BY_COLUMN_MATRIX_H_
