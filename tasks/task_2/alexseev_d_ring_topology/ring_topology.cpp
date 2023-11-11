8bd33fb0b5e59c789b2254e5520772fc5dc44a39
Danila <alexseev_dnl@mail.ru>
2023-11-11 18:46:43 +0300

    some styly change

--- tasks/task_2/alexseev_d_ring_topology/ring_topology.cpp
+++ tasks/task_2/alexseev_d_ring_topology/ring_topology.cpp
@@ -16,9 +16,9 @@ bool isClockwise(int sourceRank, int receiverRank) {
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     return (isRceiverLarger(sourceRank, receiverRank)
-	    && size - receiverRank + sourceRank < receiverRank - sourceRank)
-	    || (!isRceiverLarger(sourceRank, receiverRank)
-		&& size - sourceRank + receiverRank < sourceRank - receiverRank);
+        && size - receiverRank + sourceRank < receiverRank - sourceRank)
+        || (!isRceiverLarger(sourceRank, receiverRank)
+        && size - sourceRank + receiverRank < sourceRank - receiverRank);
 }
 
 int nextRank(int rank) {
@@ -43,7 +43,7 @@ void sendMessage(int* message, int sourceRank, int receiverRank) {
         if (rank == sourceRank) {
             MPI_Send(message, 1, MPI_INT, nextRank(rank), 0, MPI_COMM_WORLD);
         } else if ((isRceiverLarger(sourceRank, receiverRank) && rank > sourceRank && rank < receiverRank)
-			|| (!isRceiverLarger(sourceRank, receiverRank) && (rank < receiverRank || rank > sourceRank))) {
+            || (!isRceiverLarger(sourceRank, receiverRank) && (rank < receiverRank || rank > sourceRank))) {
             MPI_Recv(message, 1, MPI_INT, prevRank(rank), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             MPI_Send(message, 1, MPI_INT, nextRank(rank), 0, MPI_COMM_WORLD);
         } else if (rank == receiverRank) {
@@ -53,7 +53,7 @@ void sendMessage(int* message, int sourceRank, int receiverRank) {
         if (rank == sourceRank) {
             MPI_Send(message, 1, MPI_INT, prevRank(rank), 0, MPI_COMM_WORLD);
         } else if ((isRceiverLarger(sourceRank, receiverRank) && (rank > receiverRank || rank < sourceRank))
-			|| (!isRceiverLarger(sourceRank, receiverRank) && rank < sourceRank && rank > receiverRank)) {
+            || (!isRceiverLarger(sourceRank, receiverRank) && rank < sourceRank && rank > receiverRank)) {
             MPI_Recv(message, 1, MPI_INT, nextRank(rank), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             MPI_Send(message, 1, MPI_INT, prevRank(rank), 0, MPI_COMM_WORLD);
         } else if (rank == receiverRank) {
