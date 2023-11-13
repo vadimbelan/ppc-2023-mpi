487b34239fafbb6470b9a802126ff5775bbea626
DiPirs <admarhanck@gmail.com>
2023-11-13 15:22:45 +0300

    end

--- tasks/task_1/balyasov_num_of_alternations_signs/num_of_alternations_signs.h
+++ tasks/task_1/balyasov_num_of_alternations_signs/num_of_alternations_signs.h
@@ -4,4 +4,10 @@
 
 #include <mpi.h>
 
+#include <random>
+#include <vector>
+
+int seq_num_of_alternations_signs(const std::vector<int>& elems);
+int par_num_of_alternations_signs(const std::vector<int>& elems);
+
 #endif  // TASKS_TASK_1_BALYASOV_NUM_OF_ALTERNATIONS_SIGNS_NUM_OF_ALTERNATIONS_SIGNS_H_
