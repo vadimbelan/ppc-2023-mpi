c9c575f458477641b974df1d9131a962d74c63b2
DiPirs <admarhanck@gmail.com>
2023-11-13 19:48:29 +0300

    end

--- tasks/task_2/balyasov_reduce/main.cpp
+++ tasks/task_2/balyasov_reduce/main.cpp
@@ -3,7 +3,101 @@
 
 #include "./my_reduce.h"
 
-TEST(MPI_TESTS, Test_1) { ASSERT_EQ(1, 1); }
+TEST(MPI_TESTS, Test_double_sum) {
+  int rank = 0;
+  int world_size = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &world_size);
+
+  double value = 10.5;
+  double sum = 0;
+  int recv_rank = 0;
+  int return_code =
+      reduce(&value, &sum, 1, MPI_DOUBLE, MPI_SUM, recv_rank, comm);
+
+  if (return_code == MPI_SUCCESS && rank == recv_rank) {
+    ASSERT_EQ(sum, world_size * value);
+  }
+}
+
+TEST(MPI_TESTS, Test_float_prod) {
+  int rank = 0;
+  int world_size = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &world_size);
+
+  float value = 2;
+  float prod = 0;
+  int recv_rank = 0;
+  int return_code =
+      reduce(&value, &prod, 1, MPI_FLOAT, MPI_PROD, recv_rank, comm);
+
+  if (return_code == MPI_SUCCESS && rank == recv_rank) {
+    ASSERT_EQ(prod, std::powf(value, static_cast<float>(world_size)));
+  }
+}
+
+TEST(MPI_TESTS, Test_int_lor) {
+  int rank = 0;
+  int world_size = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &world_size);
+
+  int value = 0;
+  if (rank == 0) value = 1;
+
+  int lor = 0;
+  int recv_rank = 0;
+  int return_code = reduce(&value, &lor, 1, MPI_INT, MPI_LOR, recv_rank, comm);
+
+  if (return_code == MPI_SUCCESS && rank == recv_rank) {
+    ASSERT_EQ(lor, value);
+  }
+}
+
+TEST(MPI_TESTS, Test_int_land) {
+  int rank = 0;
+  int world_size = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &world_size);
+
+  int value = 1;
+  if (rank == 0) value = 0;
+
+  int lor = 0;
+  int recv_rank = 0;
+  int return_code = reduce(&value, &lor, 1, MPI_INT, MPI_PROD, recv_rank, comm);
+
+  if (return_code == MPI_SUCCESS && rank == recv_rank) {
+    ASSERT_EQ(lor, value);
+  }
+}
+
+TEST(MPI_TESTS, Test_error_code) {
+  int rank = 0;
+  int world_size = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &world_size);
+
+  int value = 10;
+  int sum = 0;
+  int recv_rank = world_size;
+  int return_code = reduce(&value, &sum, 1, MPI_INT, MPI_PROD, recv_rank, comm);
+
+  if (rank == 0) {
+    ASSERT_EQ(MPI_ERR_ROOT, return_code);
+  }
+}
 
 int main(int argc, char** argv) {
   int code = 0;
--- tasks/task_2/balyasov_reduce/my_reduce.h
+++ tasks/task_2/balyasov_reduce/my_reduce.h
@@ -4,4 +4,40 @@
 
 #include <mpi.h>
 
+#include <cstdlib>
+#include <cstring>
+
+template <typename T>
+void operation(void* buf, void* recvbuf, MPI_Op op) {
+  T& buf_val = *reinterpret_cast<T*>(buf);
+  T& recvbuf_val = *reinterpret_cast<T*>(recvbuf);
+
+  switch (op) {
+    case MPI_MAX:
+      if (buf_val > recvbuf_val) recvbuf_val = buf_val;
+      break;
+    case MPI_MIN:
+      if (buf_val < recvbuf_val) recvbuf_val = buf_val;
+      break;
+    case MPI_SUM:
+      recvbuf_val += buf_val;
+      break;
+    case MPI_PROD:
+      recvbuf_val *= buf_val;
+      break;
+    case MPI_LAND:
+      recvbuf_val =
+          static_cast<bool>(recvbuf_val) && static_cast<bool>(buf_val);
+      break;
+    case MPI_LOR:
+      recvbuf_val =
+          static_cast<bool>(recvbuf_val) || static_cast<bool>(buf_val);
+    default:
+      break;
+  }
+}
+
+int reduce(const void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+           MPI_Op op, int root, MPI_Comm comm);
+
 #endif  // TASKS_TASK_2_BALYASOV_REDUCE_MY_REDUCE_H_
