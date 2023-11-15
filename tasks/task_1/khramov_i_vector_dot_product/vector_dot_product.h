3aae1a5c263b17822412c4c928beab862b233a69
Khramov Ivan <khramov.vanya@bk.ru>
2023-11-15 18:56:07 +0300

    Second Commit

--- tasks/task_1/khramov_i_vector_dot_product/vector_dot_product.h
+++ tasks/task_1/khramov_i_vector_dot_product/vector_dot_product.h
@@ -6,10 +6,10 @@
 #include <string>
 
 std::vector<int> getRandomVector(int  sz);
-int getLocalVectorDotProduct(const std::vector<int>& vec1,
-                        const std::vector<int>& vec2);
+int getLocalVectorDotProduct(std::vector<int>& vec1,
+                        std::vector<int>& vec2);
 int getVectorDotProduct(boost::mpi::communicator world,
-                        const std::vector<int>& global_vec1,
-                        const std::vector<int>& global_vec2);
+                        std::vector<int>& global_vec1,
+                        std::vector<int>& global_vec2);
 
 #endif  // TASKS_TASK_1_KHRAMOV_I_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
