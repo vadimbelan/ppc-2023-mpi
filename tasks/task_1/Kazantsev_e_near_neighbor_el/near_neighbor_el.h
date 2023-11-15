93ad1b92ba70a0ebbbc7c597f39d6d1bf3afb997
neotouch1 <workgen@bk.ru>
2023-11-15 15:09:23 +0300

    ninja

--- tasks/task_1/Kazantsev_e_near_neighbor_el/near_neighbor_el.h
+++ tasks/task_1/Kazantsev_e_near_neighbor_el/near_neighbor_el.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Nesterov Alexander
-#ifndef TASKS_TASK_1_KAZANTSEV_E_MATRIX_SUM_MATRIX_SUM_H_
-#define TASKS_TASK_1_KAZANTSEV_E_MATRIX_SUM_MATRIX_SUM_H_
+#ifndef TASKS_TASK_1_KAZANTSEV_E_NEAR_NEIGHBOR_EL_NEAR_NEIGHBOR_EL_H_
+#define TASKS_TASK_1_KAZANTSEV_E_NEAR_NEIGHBOR_EL_NEAR_NEIGHBOR_EL_H_
 
 #include <vector>
 #include <string>
@@ -10,4 +10,4 @@
 std::vector<int> getRandVec(int size);
 int SecquentialCount(const std::vector<int>& vec);
 int parallelCount(const std::vector<int>& vec);
-#endif  // TASKS_TASK_1_KAZANTSEV_E_MATRIX_SUM_MATRIX_SUM_H_
+#endif  // TASKS_TASK_1_KAZANTSEV_E_NEAR_NEIGHBOR_EL_NEAR_NEIGHBOR_EL_H_
