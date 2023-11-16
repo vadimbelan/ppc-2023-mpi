7803dd0eb60f42f921d47fa5c1f9394c64bdb064
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-16 14:32:09 +0300

    Исправил ошибки линтера

--- tasks/task_1/kriseev_m_min_of_vector_elements/min_of_vector_elements.cpp
+++ tasks/task_1/kriseev_m_min_of_vector_elements/min_of_vector_elements.cpp
@@ -1,33 +1,28 @@
-#include "min_of_vector_elements.h"
+// Copyright 2023 Kriseev Mikhail
+#include "task_1/kriseev_m_min_of_vector_elements/min_of_vector_elements.h"
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/collectives.hpp>
-#include <limits.h>
-int MinOfArray(const int arr[], int size)
-{
+
+int MinOfArray(const int arr[], int size) {
     int min = INT32_MAX;
-    for(int i = 0; i < size; ++i)
-    {
-        if(arr[i] < min)
-        {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] < min) {
             min = arr[i];
         }
     }
     return min;
 }
 
-int MinElementsInVectorSequential(const std::vector<int>& vec)
-{
+int MinElementsInVectorSequential(const std::vector<int>& vec) {
     return MinOfArray(vec.data(), vec.size());
 }
 
-int MinElementsInVectorParallel(const std::vector<int>& vec)
-{
+int MinElementsInVectorParallel(const std::vector<int>& vec) {
     boost::mpi::communicator world;
     int rank = world.rank();
     int commSize = world.size();
     std::vector<int> sizes(commSize, vec.size() / commSize);
-    if(vec.size() % commSize)
-    {
+    if (vec.size() % commSize) {
         sizes.back() = vec.size() % commSize;
     }
     int localSize = sizes[rank - 0];
@@ -37,7 +32,11 @@ int MinElementsInVectorParallel(const std::vector<int>& vec)
     int localMin = MinOfArray(localData, sizes[rank]);
     int globalMin = 0;
 
-    boost::mpi::reduce(world, localMin, globalMin, boost::mpi::minimum<int>(), 0);
+    boost::mpi::reduce(
+            world,
+            localMin,
+            globalMin,
+            boost::mpi::minimum<int>(), 0);
 
     delete [] localData;
 
--- tasks/task_1/kriseev_m_min_of_vector_elements/min_of_vector_elements.h
+++ tasks/task_1/kriseev_m_min_of_vector_elements/min_of_vector_elements.h
@@ -1,10 +1,10 @@
-
-#ifndef TASKS_TASK1_KRISEEV_M_MIN_OF_VECTOR_ELEMENTS_H
-#define TASKS_TASK1_KRISEEV_M_MIN_OF_VECTOR_ELEMENTS_H
+// Copyright 2023 Kriseev Mikhail
+#ifndef TASKS_TASK_1_KRISEEV_M_MIN_OF_VECTOR_ELEMENTS_MIN_OF_VECTOR_ELEMENTS_H_
+#define TASKS_TASK_1_KRISEEV_M_MIN_OF_VECTOR_ELEMENTS_MIN_OF_VECTOR_ELEMENTS_H_
 
 #include <vector>
 
 int MinElementsInVectorSequential(const std::vector<int>& vec);
 int MinElementsInVectorParallel(const std::vector<int>& vec);
 
-#endif //TASKS_TASK1_KRISEEV_M_MIN_OF_VECTOR_ELEMENTS_H
+#endif  // TASKS_TASK_1_KRISEEV_M_MIN_OF_VECTOR_ELEMENTS_MIN_OF_VECTOR_ELEMENTS_H_
