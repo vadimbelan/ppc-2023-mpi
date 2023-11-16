825027565f10ca76680a749fbb4d1600ebca4596
Kirill23323 <KirillProkofev12345@yandex.ru>
2023-11-16 18:55:46 +0300

    attempt3

--- tasks/task_1/prokofev_k_sum_values_by_rows/sum_values_by_rows.cpp
+++ tasks/task_1/prokofev_k_sum_values_by_rows/sum_values_by_rows.cpp
@@ -1,14 +1,14 @@
 // Copyright 2023 Prokofev Kirill
-#include "task_1/prokofev_k_sum_values_by_rows/sum_values_by_rows.hpp"
+#include "task_1/prokofev_k_sum_values_by_rows/sum_values_by_rows.h"
 
-int Random(int low, int high){
+int Random(int low, int high) {
   std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dist(low, high);
 	return dist(gen);
 }
 
-std::vector<int> GenerateRandomMatrix(int n, int m){
+std::vector<int> GenerateRandomMatrix(int n, int m) {
 	if (n <= 0 || m <= 0)
 		throw "Error";
 	std::vector<int> newMatrix(n * m);
@@ -17,7 +17,7 @@ std::vector<int> GenerateRandomMatrix(int n, int m){
 	return newMatrix;
 }
 
-std::vector<int> ParallSumValuesByRows(const std::vector<int>& matrix, int n, int m){
+std::vector<int> ParallSumValuesByRows(const std::vector<int>& matrix, int n, int m) {
   int numProc;
 	MPI_Comm_size(MPI_COMM_WORLD, &numProc);
 	int rankProc;
@@ -29,13 +29,13 @@ std::vector<int> ParallSumValuesByRows(const std::vector<int>& matrix, int n, in
 		localMatrix.data(), rowsForOneProc * m, MPI_INT, 0, MPI_COMM_WORLD);
 	std::vector<int> localSum = SeqSumValuesByRows(localMatrix, rowsForOneProc, m);
 	std::vector<int>globalSum(n);
-	if (rankProc == 0){
+	if (rankProc == 0) {
 		for (int i = 0; i < n; i++)
 			globalSum[i] = 0;
 	}
 	MPI_Gather(localSum.data(), rowsForOneProc, MPI_INT, globalSum.data(), rowsForOneProc, MPI_INT, 0, MPI_COMM_WORLD);
-	if (rankProc == 0){
-		for (int i = 0; i < remainder; i++){
+	if (rankProc == 0) {
+		for (int i = 0; i < remainder; i++) {
 			for (int j = 0; j < m; j++)
 				globalSum[rowsForOneProc * numProc + i] += matrix[(rowsForOneProc * numProc + i) * m + j];
 		}
@@ -43,7 +43,7 @@ std::vector<int> ParallSumValuesByRows(const std::vector<int>& matrix, int n, in
 	return globalSum;
 }
 
-std::vector<int> SeqSumValuesByRows(const std::vector<int>& matrix, int n, int m){
+std::vector<int> SeqSumValuesByRows(const std::vector<int>& matrix, int n, int m) {
 	std::vector<int> locSum(n);
 	int s;
 	for (int i = 0; i < n; i++) {
