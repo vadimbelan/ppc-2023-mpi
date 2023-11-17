2f71ae4fd78d2d049ef7f82c8fd801012a2dc28a
Nastya <nastosimys2000@gmail.com>
2023-11-17 17:24:55 +0300

    new commit

---
+++ tasks/task_1/sredneva_a_sum_values_of_matrix_columns/sum_values.h
@@ -0,0 +1,13 @@
+// Copyright 2023 Sredneva Anastasiya
+#ifndef TASKS_TASK_1_SREDNEVA_A_SUM_VALUES_BY_MATRIX_COLUMNS_SUM_VALUES_H_
+#define TASKS_TASK_1_SREDNEVA_A_SUM_VALUES_BY_MATRIX_COLUMNS_SUM_VALUES_H_
+
+#include <vector>
+#include "mpi.h"
+
+std::vector<int> getRandomMatrix(int columns, int lines, int l, int r);
+std::vector<int> getParallelSum(const std::vector<int>& vec, int columns, int lines);
+std::vector<int> getSequentialSum(const std::vector<int>& vec, int columns);
+std::vector<int> transposeMatr(const std::vector<int>& M, int columns, int lines);
+
+#endif //TASKS_TASK_1_SREDNEVA_A_SUM_VALUES_BY_MATRIX_COLUMNS_SUM_VALUES_H_
