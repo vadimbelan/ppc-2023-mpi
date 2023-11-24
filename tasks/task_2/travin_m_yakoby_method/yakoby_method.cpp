4f41239e49d6ac9281179c58e2f981e9a5cb6164
Мfксим <stm_03@mail.ru>
2023-11-24 18:19:59 +0300

    fixed bugs

--- tasks/task_2/travin_m_yakoby_method/yakoby_method.cpp
+++ tasks/task_2/travin_m_yakoby_method/yakoby_method.cpp
@@ -5,17 +5,16 @@
 #include <boost/mpi/collectives.hpp>
 #include "task_2/travin_m_yakoby_method/yakoby_method.h"
 
-std::vector<std::vector<double>> get_rand_matrix(int size, int min, int max) {
+std::vector<double> get_rand_matrix(int size, int min, int max) {
     std::random_device dev;
     std::mt19937 gen(dev());
     std::uniform_int_distribution<int> distrib(min, max);
-    std::vector<std::vector<double>> vec =
-    std::vector<std::vector<double>>(size, std::vector<double>(size, 0));
+    std::vector<double> vec(size * size);
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            vec[i][j] = distrib(gen);
-            if ((vec[i][j] == 0) && (i == j))
-                vec[i][j]++;
+            vec[i * size + j] = distrib(gen);
+            if ((vec[i * size + j] == 0) && (i == j))
+                vec[i * size + j]++;
         }
     }
     return vec;
@@ -32,53 +31,68 @@ std::vector<double> get_rand_vector(int size, int  min, int max) {
     return vec;
 }
 
-std::vector<double> Yakobi(const std::vector<std::vector<double>>& A,
-    const std::vector<double>& b, std::vector<double> x, int num_iterations) {
+std::vector<double> Yakobi(const std::vector<double>& A,
+    const std::vector<double>& b, int num_iterations) {
     int numProc;
     MPI_Comm_size(MPI_COMM_WORLD, &numProc);
     int rankProc;
     MPI_Comm_rank(MPI_COMM_WORLD, &rankProc);
 
-    if (A[1].size() != b.size())
+    int n = b.size();
+
+    if (A.size() / n != n)
         throw std::exception();
 
     for (int i = 0; i < b.size(); i++) {
-        if (A[i][i] == 0)
+        if (A[i * n + i] == 0)
             throw std::exception();
     }
 
-    for (int i = 0; i < b.size(); ++i) {
-        MPI_Bcast(const_cast<double*>(A[i].data()),
-        b.size(),
-        MPI_DOUBLE,
-        0,
-        MPI_COMM_WORLD);
+    int chunk = b.size() / numProc;
+    int remain = b.size() % numProc;
+
+    std::vector<int> procSize(numProc, chunk * n);
+    std::vector<int> procDisp(numProc, 0);
+
+    for (int i = 0; i < numProc; i++) {
+        if (i < remain) procSize[i] += n;
+        procDisp[i] = i == 0 ? 0 : procDisp[i - 1] + procSize[i - 1];
     }
-    MPI_Bcast(const_cast<double*>(b.data()),
-             b.size(),
-             MPI_DOUBLE,
-             0,
-             MPI_COMM_WORLD);
 
-    std::vector<double> new_x(b.size());
+    std::vector<double> x(n, 0.0);
+    std::vector<double> new_x(n, 0.0);
+    int elem_amount = procSize[rankProc];
+    std::vector<double> local_a(elem_amount);
 
+    std::vector<int> vecSize(numProc, chunk);
+    std::vector<int> vecDisp(numProc, 0);
+
+    for (int i = 0; i < numProc; i++) {
+        if (i < remain) vecSize[i]++;
+        vecDisp[i] = i == 0 ? 0 : vecDisp[i - 1] + vecSize[i - 1];
+    }
+    std::vector<double> local_b(vecSize[rankProc]);
+
+    MPI_Scatterv(A.data(), procSize.data(), procDisp.data(),
+            MPI_DOUBLE, local_a.data(), elem_amount,
+            MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(b.data(), vecSize.data(), vecDisp.data(),
+        MPI_DOUBLE, local_b.data(), local_b.size(),
+        MPI_DOUBLE, 0, MPI_COMM_WORLD);
     for (int k = 0; k < num_iterations; k++) {
-        for (int i = rankProc; i < b.size(); i += numProc) {
-            new_x[i] = b[i];
-            for (int j = 0; j < b.size(); j++) {
-                if (i != j)
-                new_x[i] -= A[i][j] * x[j];
+        for (int i = 0; i < elem_amount / n; i++) {
+            new_x[i] = local_b[i];
+
+            for (int j = 0; j < n; j++) {
+                if (j != (vecDisp[rankProc] + i))
+                    new_x[i] -= local_a[i * n + j] * x[j];
             }
-            new_x[i] /= A[i][i];
+            new_x[i] /= local_a[i * n + vecDisp[rankProc] + i];
         }
 
-        MPI_Allgather(new_x.data() + rankProc * (b.size() / numProc),
-            b.size() / numProc,
-            MPI_DOUBLE,
-            x.data(),
-            b.size() / numProc,
-            MPI_DOUBLE,
-            MPI_COMM_WORLD);
+        MPI_Allgatherv(new_x.data(), local_b.size(), MPI_DOUBLE,
+            x.data(), vecSize.data(), vecDisp.data(),
+            MPI_DOUBLE, MPI_COMM_WORLD);
     }
     return x;
 }
--- tasks/task_2/travin_m_yakoby_method/yakoby_method.h
+++ tasks/task_2/travin_m_yakoby_method/yakoby_method.h
@@ -3,10 +3,10 @@
 #define TASKS_TASK_2_TRAVIN_M_YAKOBY_METHOD_YAKOBY_METHOD_H_
 #include <vector>
 
-std::vector<double> Yakobi(const std::vector<std::vector<double>>& A,
+std::vector<double> Yakobi(const std::vector<double>& A,
     const std::vector<double>& b,
-std::vector<double> x, int num_iterations = 100);
-std::vector<std::vector<double>> get_rand_matrix(int size,
+    int num_iterations = 10);
+std::vector<double> get_rand_matrix(int size,
     int min = -1000, int max = -1000);
 std::vector<double> get_rand_vector(int size, int min = -1000, int max = 1000);
 #endif  // TASKS_TASK_2_TRAVIN_M_YAKOBY_METHOD_YAKOBY_METHOD_H_
