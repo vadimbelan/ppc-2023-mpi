15dbe5ffc416cacb6e5c06c34a54ec5537e20e0f
bliiinch <114909814+bliiinch@users.noreply.github.com>
2023-12-03 00:45:01 +0300

    make processes work more parallel

--- tasks/task_2/lysanova_j_rdrs_wrtrs/rdrs_wrtrs.cpp
+++ tasks/task_2/lysanova_j_rdrs_wrtrs/rdrs_wrtrs.cpp
@@ -1,8 +1,21 @@
 // Copyright 2023 Lysanova Julia
+#include <utility>
+#include <unordered_map>
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/collectives.hpp>
 #include "task_2/lysanova_j_rdrs_wrtrs/rdrs_wrtrs.h"
 
+#define SUPERVIZOR 0
+const enum TAGS {
+    READY,
+    DATA,
+    DONE
+};
+
+const enum ROLES {
+    READER,
+    WRITER
+};
 
 
 std::pair<std::queue<std::pair<int, int>>, std::vector<int>> rdrs_wrtrs_boost() {
@@ -10,31 +23,62 @@ std::pair<std::queue<std::pair<int, int>>, std::vector<int>> rdrs_wrtrs_boost() 
     int shared_data = 0, role;
 
     if (world.rank() == 0) {
-        std::queue<std::pair<int, int>> q;
-        for (int i = 0; i < world.size() - 1; i++) {
-            auto status = world.recv(MPI_ANY_SOURCE, 0, role);
-            q.push({status.source(), role});
-            if (role == 1) {
-                world.recv(status.source(), 1, shared_data);
-            } else {
-                world.send(status.source(), 1, shared_data);
+        std::unordered_map<int, boost::mpi::request> working_readers;
+        std::queue<int> ready_writers;
+        std::queue<std::pair<int, int>> serve_order;
+        boost::mpi::status status;
+        int process = world.size() - 1, readers_count = 0;
+        while (process) {
+            for (auto &i : working_readers) {
+                if (i.second.test()) {
+                    serve_order.push({i.first, READER});
+                    readers_count--;
+                    process--;
+                    working_readers.erase(i.first);
+                }
             }
+            if (readers_count == 0 && ready_writers.size() > 0) {
+                world.recv(ready_writers.front(), DATA, shared_data);
+                serve_order.push({ready_writers.front(), WRITER});
+                ready_writers.pop();
+                process--;
+            }
+            if (world.iprobe(MPI_ANY_SOURCE, READY).has_value()) {
+                status = world.recv(MPI_ANY_SOURCE, READY, role);
+                if (role == WRITER) {
+                    if (readers_count) {
+                        ready_writers.push(status.source());
+                    } else {
+                        world.recv(status.source(), DATA, shared_data);
+                        serve_order.push({status.source(), WRITER});
+                        process--;
+                    }
+                } else {
+                    readers_count++;
+                    world.send(status.source(), DATA, shared_data);
+                    working_readers[status.source()] = world.irecv(status.source(), DONE);
+                }
+            }
+
         }
+
+
         std::vector<int> datas(world.size());
-        boost::mpi::gather(world, shared_data, datas, 0);
+        boost::mpi::gather(world, shared_data, datas, SUPERVIZOR);
         datas[0] = shared_data;
         world.barrier();
-        return {q, datas};
+        return {serve_order, datas};
     } else {
         role = world.rank() % 2;
-        world.send(0, 0, role);
-        if (role == 1) {
+        world.send(SUPERVIZOR, READY, role);
+        if (role == WRITER) {
             shared_data = world.rank() * 10;
-            world.send(0, 1, shared_data);
+            world.send(SUPERVIZOR, DATA, shared_data);
         } else {
-            world.recv(0, 1, shared_data);
+            world.recv(SUPERVIZOR, DATA, shared_data);
+            world.send(SUPERVIZOR, DONE);
         }
-        boost::mpi::gather(world, shared_data, 0);
+        boost::mpi::gather(world, shared_data, SUPERVIZOR);
         world.barrier();
         return {{}, {}};
     }
