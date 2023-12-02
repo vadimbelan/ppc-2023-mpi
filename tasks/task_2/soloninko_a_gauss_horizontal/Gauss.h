b4bd5096490a5ea05876a17cd68966e451809ba5
Andrey Soloninko <soloninko80@gmail.com>
2023-12-02 17:29:53 +0300

    initial commit

---
+++ tasks/task_2/soloninko_a_gauss_horizontal/Gauss.h
@@ -0,0 +1,30 @@
+// Copyright 2023 Soloninko Andrey
+#ifndef TASKS_TASK_2_SOLONINKO_A_GAUSS_HORIZONTAL_GAUSS_H_
+#define TASKS_TASK_2_SOLONINKO_A_GAUSS_HORIZONTAL_GAUSS_H_
+
+#include <mpi.h>
+
+#include <iostream>
+#include <random>
+#include <vector>
+
+bool check_res(std::vector<double>& matrix, std::vector<double>& x, int size);
+
+void rand_matr(std::vector<double>& matrix, int size);
+
+void init_matr_res(std::vector<double>& matrix, std::vector<int>& rows,
+                   std::vector<double>& res, int row, int size);
+
+void gaussian_elimination(std::vector<double>& matrix, std::vector<int>& rows,
+                          int size, int rank, int comm_size, int row,
+                          int nrows);
+
+void gaussian_substitution(std::vector<double>& matrix,
+                           std::vector<double>& res, std::vector<int>& rows,
+                           int row, int nrows, int rank, int comm_size,
+                           int size);
+
+void gauss_algo(std::vector<double>& matrix, std::vector<double>& res,
+                int size);
+
+#endif  // TASKS_TASK_2_SOLONINKO_A_GAUSS_HORIZONTAL_GAUSS_H_
