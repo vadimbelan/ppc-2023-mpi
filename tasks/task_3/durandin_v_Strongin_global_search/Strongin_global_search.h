5ed86abf196526e97e1cd6c50c9d355ed9f12b69
Sturmannn <vladimir-ych@yandex.ru>
2023-12-17 19:22:13 +0300

    fix_codestyle

--- tasks/task_3/durandin_v_Strongin_global_search/Strongin_global_search.h
+++ tasks/task_3/durandin_v_Strongin_global_search/Strongin_global_search.h
@@ -3,17 +3,16 @@
 #ifndef TASKS_TASK_3_DURANDIN_V_STRONGIN_GLOBAL_SEARCH_STRONGIN_GLOBAL_SEARCH_H_
 #define TASKS_TASK_3_DURANDIN_V_STRONGIN_GLOBAL_SEARCH_STRONGIN_GLOBAL_SEARCH_H_
 
+#include <mpi.h>
 #include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <random>
 #include <vector>
-#include <mpi.h>
-
 
 namespace global_search {
 double seq_Strongin_alghorithm(double x0, double x1, double eps);
 double paral_Strongin_algorithm(double x0, double x1, double eps);
-} // namespace global_search
+}  // namespace global_search
 
 #endif  // TASKS_TASK_3_DURANDIN_V_STRONGIN_GLOBAL_SEARCH_STRONGIN_GLOBAL_SEARCH_H_
