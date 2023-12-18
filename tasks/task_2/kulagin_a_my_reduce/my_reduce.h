3b21016c88d34119bf485269dd111af4517e8445
mahbhlddnhakkh <99867842+mahbhlddnhakkh@users.noreply.github.com>
2023-12-18 19:40:58 +0300

    Кулагин Александр. Задача 2. Вариант 6. Передача от всех одному (reduce). (#81)
    * first try
    
    * make fixed size vector in tests
    
    * First try to fix the weird error with MPI_Scatter
    
    * Use tree

---
+++ tasks/task_2/kulagin_a_my_reduce/my_reduce.h
@@ -0,0 +1,16 @@
+// Copyright 2023 kulagin_a
+#ifndef TASKS_TASK_2_KULAGIN_A_MY_REDUCE_MY_REDUCE_H_
+#define TASKS_TASK_2_KULAGIN_A_MY_REDUCE_MY_REDUCE_H_
+
+#include <mpi.h>
+
+struct proc_tree_props {
+  int parent = -1;
+  int children[2] = { -1, -1 };
+};
+
+void calculate_tree_props(int proc_rank, int proc_num, proc_tree_props* props, int* path = NULL, int* path_size = NULL);
+
+int my_mpi_reduce(const void* sendbuf, void* recvbuf, int count, MPI_Datatype type, MPI_Op op, int root, MPI_Comm comm);
+
+#endif  // TASKS_TASK_2_KULAGIN_A_MY_REDUCE_MY_REDUCE_H_
