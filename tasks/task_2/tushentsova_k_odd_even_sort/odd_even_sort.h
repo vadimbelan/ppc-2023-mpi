743e8a39d55960be40fee6286dea3109ff2378cf
Карина Тушенцова <103189242+karrin0chka@users.noreply.github.com>
2023-12-26 01:31:19 +0300

    Тушенцова Карина. Задача 2. Вариант 25. Сортировка пузырьком (#401)
    * Тушенцова Карина. Задача 2. Вариант 25. Сортировка пузырьком (алгоритм чет-нечетной перестановки)
    
    * Тушенцова Карина. Задача 2. Вариант 25. Сортировка пузырьком

---
+++ tasks/task_2/tushentsova_k_odd_even_sort/odd_even_sort.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Tushentsova Karina
+#ifndef TASKS_TASK_2_TUSHENTSOVA_K_ODD_EVEN_SORT_ODD_EVEN_SORT_H_
+#define TASKS_TASK_2_TUSHENTSOVA_K_ODD_EVEN_SORT_ODD_EVEN_SORT_H_
+
+#include <vector>
+
+std::vector<int> getRandomVector(int size);
+std::vector<int> bubbleSortOddEven(std::vector<int> vec);
+std::vector<int> parallBubbleSortOddEven(std::vector<int> vec);
+
+#endif  // TASKS_TASK_2_TUSHENTSOVA_K_ODD_EVEN_SORT_ODD_EVEN_SORT_H_
