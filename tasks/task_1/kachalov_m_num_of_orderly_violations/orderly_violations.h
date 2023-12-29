ee7f6dc8ab3a90b8857eff46c16e660d4dbde834
KMV <85839152+KachalovM@users.noreply.github.com>
2023-12-29 17:45:57 +0300

    Качалов Михаил. Задача 1. Вариант 6. Нахождение числа нарушений упорядоченности соседних элементов вектора. (#472)
    * Create task 1
    
    * Changed task 1
    
    * Changed task 1

---
+++ tasks/task_1/kachalov_m_num_of_orderly_violations/orderly_violations.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Kachalov Mikhail
+#ifndef TASKS_TASK_1_KACHALOV_M_NUM_OF_ORDERLY_VIOLATIONS_ORDERLY_VIOLATIONS_H_
+#define TASKS_TASK_1_KACHALOV_M_NUM_OF_ORDERLY_VIOLATIONS_ORDERLY_VIOLATIONS_H_
+
+#include <vector>
+
+int num_of_orderly_violations(const std::vector<int>& vec);
+int parallel_num_of_orderly_violations(const std::vector<int>& vec);
+std::vector<int> get_random_vector(const int& length);
+
+#endif  // TASKS_TASK_1_KACHALOV_M_NUM_OF_ORDERLY_VIOLATIONS_ORDERLY_VIOLATIONS_H_
