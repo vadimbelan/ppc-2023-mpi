f29417884a13bf8171dccd1980cc5bc9316d274e
alesyacurtis <154752137+alesyacurtis@users.noreply.github.com>
2024-01-14 05:24:18 +0300

    Шемякина Алеся. Задача 1. Вариант 8. Нахождение наиболее отличающихся по значению соседних элементов вектора. (#493)
    * second_commit
    
    * third_commit
    
    * fourth_commit

---
+++ tasks/task_1/shemyakina_a_most_different_neighbor_elements/most_different_neighbor_elements.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Shemyakina Alesya
+#ifndef TASKS_TASK_1_SHEMYAKINA_A_MOST_DIFFERENT_NEIGHBOR_ELEMENTS_MOST_DIFFERENT_NEIGHBOR_ELEMENTS_H_
+#define TASKS_TASK_1_SHEMYAKINA_A_MOST_DIFFERENT_NEIGHBOR_ELEMENTS_MOST_DIFFERENT_NEIGHBOR_ELEMENTS_H_
+
+#include <vector>
+
+std::vector<int> VecRandNum(int vector_size, int min_value, int max_value);
+int SequentialGet(const std::vector<int> &vec);
+int ParallelGet(const std::vector<int> &vec);
+
+#endif  // TASKS_TASK_1_SHEMYAKINA_A_MOST_DIFFERENT_NEIGHBOR_ELEMENTS_MOST_DIFFERENT_NEIGHBOR_ELEMENTS_H_
